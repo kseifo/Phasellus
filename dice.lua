923bebc1ab5b993209152984c3044941080817d3
Kareem Seifo <kareem.seifo@student.manchester.ac.uk>
2025-07-31 01:18:40 +0100

    Created proper dice class layout

--- dice.lua
+++ dice.lua
@@ -1,7 +1,25 @@
 local Dice = {}
+Dice.__index = Dice
 
-function Dice.rollDice()
-    return math.random(1,6)
+-- Constructor
+function Dice.new()
+    local self = setmetatable({}, Dice)
+    self.value = math.random(1, 6)
+    return self
+end
+
+-- Setters and Getters
+function Dice:setValue(value)
+    self.value = value
+end
+
+function Dice:getValue()
+    return self.value
+end
+
+-- Roll dice function
+function Dice:roll()
+    self.value = math.random(1, 6) -- Roll the die to get a new random value
 end
 
 return Dice
