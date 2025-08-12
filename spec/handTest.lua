62ae8664bc6c843832d4086077d80f83b947c5bc
Kareem Seifo <kareem.seifo@student.manchester.ac.uk>
2025-08-12 21:38:30 +0100

    Added test to check move functions

--- spec/handTest.lua
+++ spec/handTest.lua
@@ -15,4 +15,15 @@ function testGetAllDice()
     luaunit.assertEquals(#h:getAllDice(), 5)
 end
 
+function testMoveToHold()
+    local h = hand.new()
+    h:moveToHold(h.reroll[2])
+    luaunit.assertEquals(#h.hold, 1)
+    luaunit.assertEquals(#h.reroll, 4)
+
+    h:moveToHold(h.reroll[1])
+    luaunit.assertEquals(#h.hold, 2)
+    luaunit.assertEquals(#h.reroll, 3)
+end
+
 os.exit(luaunit.LuaUnit.run())
